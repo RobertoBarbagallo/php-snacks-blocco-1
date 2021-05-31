3a5fde92c66312942caa19cfff49c677a5aa62f2
RobertoBarbagallo <roberto-barbagallo@live.it>
2021-05-31 21:03:57 +0200

    consegna

---
+++ index3.php
@@ -0,0 +1,90 @@
+<!-- ## Snack 3
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post. -->
+
+<?php
+
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Posts</title>
+</head>
+
+<body>
+    <?php
+
+
+    $post_keys = array_keys($posts);
+    for ($i = 0; $i < count($post_keys); $i++) {
+        $date = $post_keys[$i];
+        $array = $posts[$date];
+    ?>
+
+        <h2><?php echo $date  ?></h2>
+
+        <?php for ($j = 0; $j < count($array); $j++) {
+            $post = $array[$j];
+
+        ?>
+            <h3><?php echo $post["title"] ?></h3>
+            <p><strong><?php echo $post["author"] ?></strong></p>
+            <p><?php echo $post["text"] ?></p>
+            <br>
+        <?php
+        }
+        ?>
+
+
+    <?php
+    }
+    ?>
+</body>
+
+</html>
